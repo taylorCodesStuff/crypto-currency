c6ceb32b592ee9087deba2d59be8588f0e408af9
Taylor Hammons <taylor@thinkful.com>
2018-11-30 11:19:08 -0600

    add styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Welcome from './Welcome';
 import './App.css';
+import styled from 'styled-components';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Welcome />
     );
   }
 }
