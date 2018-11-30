8c4cadd3cf6ad5b038610601ec3bd137e2569d13
Taylor Hammons <taylor@thinkful.com>
2018-11-30 15:04:12 -0600

    add CoinGrid

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Welcome from './Welcome';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 export default function() {
     return (
         <Page name="Settings">
             <Welcome />
             <ConfirmButton />
+            <CoinGrid />
         </Page>
     );
 }
