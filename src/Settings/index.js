824b6232c3c32d1e5be97dede7b38853674035e5
Taylor Hammons <taylor@thinkful.com>
2018-11-30 14:18:52 -0600

    add Page component

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Welcome from './Welcome';
 import ConfirmButton from './ConfirmButton';
+import Page from '../Shared/Page';
 
 export default function() {
     return (
-        <div>
+        <Page name="Settings">
             <Welcome />
             <ConfirmButton />
-        </div>
-    )
+        </Page>
+    );
 }
