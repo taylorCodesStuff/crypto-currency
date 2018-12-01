29ae65996f5001db676a810ca33d8d45d39eb015
Taylor Hammons <taylor@thinkful.com>
2018-11-30 19:11:21 -0600

    create coingrid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {AppContext} from '../App/AppProvider';
-
+import {Tile, SelectableTile} from '../Shared/Tile';
 
 const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
+    grid-gap: 14px;
 `
 
 export default function() {
@@ -14,7 +15,7 @@ export default function() {
             {({coinList}) => (
             <CoinGridStyled>
                 {Object.keys(coinList).map((coinKey, index) => 
-                    <div key={index}> {coinKey} </div>     
+                    <SelectableTile key={index}> {coinKey} </SelectableTile>     
                 )}
             </CoinGridStyled> )}
         </AppContext.Consumer>  
